08bf5d7fd1c96d239ee117ecc172060a7697115c
cychen <20373737@buaa.edu.cn>
2023-10-25 20:11:01 +0800

    [exercises](tests)

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -31,8 +31,12 @@ unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
+    /// # Safety
+    /// The `address` must contain a mutable reference to a valid `u32` value.
+    /// We assign `0xAABBCCDD` to the value referred by `address`.
     unsafe {
-        todo!("Your code goes here")
+        // todo!("Your code goes here")
+        *(address as *mut u32) = 0xAABBCCDD;
     }
 }
 
